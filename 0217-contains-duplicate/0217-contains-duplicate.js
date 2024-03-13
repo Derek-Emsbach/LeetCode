0fcb820d45169f30e8c120d9643edba13c231184
Derek Emsbach <37425403+Derek-Emsbach@users.noreply.github.com>
2024-03-12 22:44:01 -0600

    Time: 75 ms (77.14%), Space: 62.5 MB (39.96%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,15 +2,27 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// var containsDuplicate = function(nums) {
+// //     Sort nums using .sort()
+//     const sortedNums = nums.sort()
+//     console.log(sortedNums)
+// //     Iterate and see if next number is equal -> return True
+//     for (let i = 0; i < sortedNums.length; i++) {
+//         if(nums[i] === nums[i + 1]) {
+//             return true
+//         }
+//     }
+//     return false
+// };
+
 var containsDuplicate = function(nums) {
-//     Sort nums using .sort()
-    const sortedNums = nums.sort()
-    console.log(sortedNums)
-//     Iterate and see if next number is equal -> return True
-    for (let i = 0; i < sortedNums.length; i++) {
-        if(nums[i] === nums[i + 1]) {
+    let numSet = new Set()
+    
+    for(let num of nums) {
+        if(numSet.has(num)) {
             return true
         }
+        numSet.add(num)
     }
     return false
 };
