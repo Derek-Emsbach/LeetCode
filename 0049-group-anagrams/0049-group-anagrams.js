ad221406882906e1991707cc72de45fef2563771
Derek Emsbach <37425403+Derek-Emsbach@users.noreply.github.com>
2024-04-26 22:09:33 -0600

    Time: 106 ms (85.73%), Space: 62.3 MB (74.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,22 +3,22 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    const anagrams = {};
+    const anagrams = new Map();
     
     // Iterate through each string in the array
     for (const str of strs) {
         // Sort the characters of the string to get its signature
         const sortedStr = str.split('').sort().join('');
         
-        // If the signature already exists in the object, push the string to its array
-        if (anagrams[sortedStr]) {
-            anagrams[sortedStr].push(str);
+        // If the signature already exists in the map, push the string to its array
+        if (anagrams.has(sortedStr)) {
+            anagrams.get(sortedStr).push(str);
         } else {
             // If the signature doesn't exist, create a new entry with the sorted string as key
-            anagrams[sortedStr] = [str];
+            anagrams.set(sortedStr, [str]);
         }
     }
     
-    // Convert the object values (arrays of anagrams) to an array and return
-    return Object.values(anagrams);
+    // Convert the map values (arrays of anagrams) to an array and return
+    return Array.from(anagrams.values());
 };
